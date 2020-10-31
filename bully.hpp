03769124671378f8a4ca948df6053ab6d606ea67
Kevork <kevorkchristian@gmail.com>
2020-10-31 21:35:24 +0100

    V0.2

--- bully.hpp
+++ bully.hpp
@@ -9,19 +9,27 @@ enum state   {OFFLINE=0, ONLINE, IN_ELECTION, LEADER};
 class Bully {
     private:
         static int message_no;
+        static int curr_leader_ID;
+        static vector<Bully> node_list;
         int ID;
         state st;
 
-        int curr_leader_ID;
     public:
         Bully(int ID);
         void send_message(message msg, Bully* receiver);
-        void received(message msg);
+        void receive(message msg);
         void set_state(state st);
         int get_ID();
+        int get_leader();
 };
 
+// Temporary test method
+int Bully::get_leader() {
+    return curr_leader_ID;
+}
+
 int Bully::message_no = 0;
+int Bully::curr_leader_ID = -1;
 
 Bully::Bully(int ID) {
     this->st = ONLINE;
@@ -37,39 +45,29 @@ void Bully::set_state(state st) {
 }
 
 void Bully::send_message(message msg, Bully* receiver) {
-    if (this->st == ONLINE) {
+    if (receiver->st == ONLINE) {
         message_no++;
-        switch(msg) {
-            case ELECTION:
-                receiver->received(msg);
-                receiver->set_state(IN_ELECTION);
-                receiver->send_message(OK, this);
-                break;
-            case OK:
-                receiver->received(msg);
-                receiver->set_state(ONLINE);
-                break;
-            case COORDINATOR:
-                receiver->received(msg);
-                receiver->set_state(ONLINE);
-                curr_leader_ID = this->ID;
-                break;
-            default:
-                break;
+        if (msg == COORDINATOR) {
+            curr_leader_ID = this->ID;
         }
+        receiver->receive(msg);
     }
     return;
 }
 
-void Bully::received(message msg) {
+void Bully::receive(message msg) {
     switch(msg) {
         case ELECTION:
+            this->st = IN_ELECTION;
+            this->send_message(OK, this);
             cout << "ID: " << this->ID << " received ELECTION" << endl;
             break;
         case OK:
+            this->st = ONLINE;
             cout << "ID: " << this->ID << " received OK" << endl;
             break;
         case COORDINATOR:
+            this->st = ONLINE;
             cout << "ID: " << this->ID << " received COORDINATOR" << endl;
             break;
         default:
--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include "bully.hpp"
 
+/**
+ * TO-DO:
+ * 1. Compare ID's
+ * 2. Network interface with bully class friend
+ * 3. Vector of bullies - Possible in network interface
+ * 4. The Leader Election algoritm
+ * 
+ * TO-DO: Optional
+ * 1. Interpreter - SO COOL
+ * 2. Implement nodes on threads
+ * 3. Bully.run() function for thread
+ */
+
 int main() {
     Bully* b0 = new Bully(0);
     Bully* b1 = new Bully(1);
 
     b0->send_message(ELECTION, b1);
+    b1->send_message(COORDINATOR, b0);
+
+    cout << "Leader ID: " << b0->get_leader() << endl;
 
     return 0;
 }
