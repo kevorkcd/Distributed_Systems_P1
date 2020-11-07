e161e66854a572e74d8e5e9a05ac2d78b0d171f8
Kevork <kevork@users.noreply.github.com>
2020-11-07 18:23:41 +0100

    Made test.cpp more reliable

--- test.cpp
+++ test.cpp
@@ -4,16 +4,23 @@
 int main() {
     {
         SingletonBully* network = SingletonBully::get_instance();
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         network->make_nodes(10);
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         network->boot_nodes();
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        std::this_thread::sleep_for(std::chrono::milliseconds(1500));
         network->shutdown(6);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
         network->fail(5);
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
         network->fail(4);
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         network->fail(1);
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         network->shutdown(2);
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         network->shutdown(10);
-        std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+        std::this_thread::sleep_for(std::chrono::milliseconds(3000));
         network->list_nodes();
         BullyCLI* UI = new BullyCLI();
         UI->run();
